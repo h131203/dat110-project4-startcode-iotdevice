e7562841c23ad9ad591650dcbdc4c029b6aacb09
h_gri <h_gri@HGLaptop5>
2022-04-28 18:33:45 +0200

    HTTP greier OK

--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -1,5 +1,16 @@
 package no.hvl.dat110.aciotdevice.client;
 
+import java.io.IOException;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class RestClient {
 
 	public RestClient() {
@@ -11,6 +22,28 @@ public class RestClient {
 	public void doPostAccessEntry(String message) {
 
 		// TODO: implement a HTTP POST on the service to post the message
+//		.... which is to issue a HTTP GET request on the cloud service in order to obtain the current access code. 
+//		This method will be called from the loop-function in the AccessController.java class 
+//		immediately before checking whether a provided access code is valid, i.e., in state CHECKING.
+		
+		//første test med OkHTTP
+		OkHttpClient okClient = new OkHttpClient();
+		Gson gson = new Gson();
+		AccessMessage msg = new AccessMessage(message);
+
+		MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+		RequestBody reqBody = RequestBody.create(JSON, gson.toJson(msg));
+
+		Request request = new Request.Builder()
+				.url("http://localhost:8080" + logpath)
+				.post(reqBody)
+				.build();
+
+		try (Response response = okClient.newCall(request).execute()) {
+			System.out.println(response.body().string());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
@@ -21,6 +54,25 @@ public class RestClient {
 		AccessCode code = null;
 		
 		// TODO: implement a HTTP GET on the service to get current access code
+	
+		//TEST 1
+		
+		OkHttpClient okClient = new OkHttpClient();
+		Gson gson = new Gson();
+
+		Request request = new Request.Builder()
+				.url("http://localhost:8080/" + codepath)
+				.get()
+				.build();
+
+
+		try (Response response = okClient.newCall(request).execute()) {
+	
+			code = gson.fromJson(response.body().string(), AccessCode.class);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		return code;
 	}
